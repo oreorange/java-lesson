6cee2a1442a8abd4f7cd067725e6bce19bcdec7b
oreorange <92431652+oreorange@users.noreply.github.com>
2022-02-23 05:24:18 +0900

    [Finish]7

--- src/chapter06.java
+++ src/chapter06.java
@@ -2,7 +2,7 @@
 public class chapter06 {
 	public static void main(String[] args) {
 		int val;
-		val = 2 + 5;
+		val = 2 + 5;  // int val = 2 + 5;
 		System.out.println(val);
 
 		val++;
@@ -14,8 +14,7 @@ public class chapter06 {
 		boolean bool;
 		bool = val == 50;
 		System.out.println(bool);
-		bool = (val < 50) && (val % 10 == 0);
+		bool = (val < 50) && (val % 10 == 0);  // 比較するときは（）
 		System.out.println(bool);
 	}
-
 }
