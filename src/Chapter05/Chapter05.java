b14ddf7625763371cba816b06d0a5c5e70b4a1fb
oreorange <92431652+oreorange@users.noreply.github.com>
2022-02-22 18:08:14 +0900

    [Finish]5

--- src/Chapter05/Chapter05.java
+++ src/Chapter05/Chapter05.java
@@ -2,6 +2,7 @@ package Chapter05;
 
 public class Chapter05 {
 	public static void main(String[] args) {
+//		プリミティブ型のlong double boolean
 		long long1 = 1234567L;
 		double double1 = 1234.56D;
 		boolean boolean1 = true;
@@ -9,6 +10,16 @@ public class Chapter05 {
 		System.out.println(double1);
 		System.out.println(boolean1);
 		
+		
+		Float float1 = 0.123F;
+		System.out.println(float1);
+		Integer i = 100;
+		System.out.println(i);
+		Character c = 'う';
+		System.out.println(c);
+		
+		int[] array1 = new int[] { 10,20,30,40,50}; //配列の宣言と生成　5つの要素
+		System.out.println(array1[2]); //30出力
+		
 	}
-
 }
